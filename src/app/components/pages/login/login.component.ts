053c6e3d5213c91454e53dd9de18f29251f2f432
Elio Neto <netoo.elio@hotmail.com>
2021-10-09 15:19:42 -0300

    login screen ended

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { UserModel } from 'src/app/models/userModel';
 
 @Component({
@@ -15,15 +16,32 @@ export class LoginComponent implements OnInit {
     email: 'test@test.com',
     passwd: 'passwd',
   };
-  constructor() {}
+
+  showEmail: boolean = true;
+  showPassWd: boolean = false;
+
+  constructor(private route:Router) {}
 
   ngOnInit(): void {}
-  testEmail(): boolean {
+  testEmail(): void {
     console.log(this.user.email);
     if (this.user.email === this.validUser.email) {
-      return true;
+      console.log('ok');
+      this.showEmail = false;
+      this.showPassWd = true;
     } else {
-      return false;
+      console.log('fail');
+    }
+  }
+  testPasswd(): void {
+    console.log(this.user.passwd);
+    if (this.user.passwd === this.validUser.passwd) {
+      console.log('ok');
+      this.showEmail = false;
+      this.showPassWd = true;
+this.route.navigate(['/'])
+    } else {
+      console.log('fail');
     }
   }
 }
