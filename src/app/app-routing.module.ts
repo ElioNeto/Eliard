a5716ec61eafa1c2754c553e2e34fae698e243f5
Elio Neto <netoo.elio@hotmail.com>
2021-10-09 19:31:26 -0300

    reorder diretory structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/pages/login/login.component';
-import { SignupComponent } from './components/pages/signup/signup.component';
-import { HomeComponent } from './components/pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
